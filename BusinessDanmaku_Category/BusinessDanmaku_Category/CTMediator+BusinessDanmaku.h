bc61569e61c5840880ad521c6cd01eeb281ddec2
zhaoyang <wustzhy@sina.com>
2025-09-28 22:23:35 +0800

    feat: Category方法 增加参数

--- BusinessDanmaku_Category/BusinessDanmaku_Category/CTMediator+BusinessDanmaku.h
+++ BusinessDanmaku_Category/BusinessDanmaku_Category/CTMediator+BusinessDanmaku.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CTMediator (BusinessDanmaku)
 
-- (UIViewController *)BusinessDanmaku_aViewController;
+- (UIViewController *)BusinessDanmaku_aViewControllerWithParams:(NSDictionary *)params;
 
 
 @end
